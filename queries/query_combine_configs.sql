121ef760fdea86aaa5b1fd8f2ce4cb9dd5f091f6
hughb-fs <156920457+hughb-fs@users.noreply.github.com>
2024-10-11 20:16:38 +0100

    first commit - copy from ad_hoc project

--- queries/query_combine_configs.sql
+++ queries/query_combine_configs.sql
@@ -3,7 +3,7 @@ with rollup67 as (
 select * except (rps)
 from `streamamp-qa-239417.DAS_increment.DAS_config_combined_uncompressed_2024-10-10_2_1_1`
 where config_level in (6, 7)
-qualify min(config_level) over(partition by date, geo_continent, device_category, country_code, domain, bidders) >= config_level
+qualify min(config_level) over(partition by date, geo_continent, device_category, country_code, domain, bidders) >= config_level or config_level != 7
 
 ), rollup567 as (
 
@@ -16,7 +16,7 @@ select * from (
   from `streamamp-qa-239417.DAS_increment.DAS_config_combined_uncompressed_2024-10-10_2_1_1`
   where config_level = 5
 )
-qualify min(config_level) over(partition by date, geo_continent, country_code, domain, bidders) >= config_level
+qualify min(config_level) over(partition by date, geo_continent, country_code, domain, bidders) >= config_level or config_level != 6
 
 ), rollup4567 as (
 
@@ -30,7 +30,7 @@ from (
   from `streamamp-qa-239417.DAS_increment.DAS_config_combined_uncompressed_2024-10-10_2_1_1`
   where config_level = 4
 )
-qualify min(config_level) over(partition by date, geo_continent, device_category, rtt_category, country_code, bidders) >= config_level
+qualify min(config_level) over(partition by date, geo_continent, device_category, rtt_category, country_code, bidders) >= config_level or config_level != 5
 
 ), rollup34567 as (
 
@@ -44,7 +44,7 @@ from (
   from `streamamp-qa-239417.DAS_increment.DAS_config_combined_uncompressed_2024-10-10_2_1_1`
   where config_level = 3
 )
-qualify min(config_level) over(partition by date, geo_continent, device_category, country_code, bidders) >= config_level
+qualify min(config_level) over(partition by date, geo_continent, device_category, country_code, bidders) >= config_level or config_level != 4
 
 ), rollup234567 as (
 
@@ -58,7 +58,7 @@ from (
   from `streamamp-qa-239417.DAS_increment.DAS_config_combined_uncompressed_2024-10-10_2_1_1`
   where config_level = 2
 )
-qualify min(config_level) over(partition by date, geo_continent, country_code, bidders) >= config_level
+qualify min(config_level) over(partition by date, geo_continent, country_code, bidders) >= config_level or config_level != 3
 
 ), rollup1234567 as (
 
@@ -72,7 +72,7 @@ from (
   from `streamamp-qa-239417.DAS_increment.DAS_config_combined_uncompressed_2024-10-10_2_1_1`
   where config_level = 1
 )
-qualify min(config_level) over(partition by date, geo_continent, country_code, bidders) >= config_level
+qualify min(config_level) over(partition by date, geo_continent, bidders) >= config_level or config_level != 2
 ), rollup01234567 as (
 
 select *
@@ -85,8 +85,9 @@ from (
   from `streamamp-qa-239417.DAS_increment.DAS_config_combined_uncompressed_2024-10-10_2_1_1`
   where config_level = 0
 )
-qualify min(config_level) over(partition by date, geo_continent, bidders) >= config_level
+qualify min(config_level) over(partition by date, bidders) >= config_level or config_level != 1
 )
 
-select * from rollup01234567
+select *
+from rollup01234567
 
